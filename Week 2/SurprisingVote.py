7f061732d88699e3f8404c51aece59a7613a8f8f
/ᐠ. ｡.ᐟ\ᵐᵉᵒʷˎˊ˗ <thanapon9114@gmail.com>
2025-08-22 00:01:38 +0700

    ..

--- Week 2/SurprisingVote.py
+++ Week 2/SurprisingVote.py
@@ -1,7 +1,7 @@
 """SurprisingVote"""
 S = float(input())
 Mx = float(input())
-if Mx - ((S - Mx) // 2)>= 2 :
+if Mx - ((S - Mx) // 2) >= 2 :
     a = [Mx, S % Mx, S - (S % Mx)]
     if max(a) - min(a) >= 2 :
         print("Surprising")
